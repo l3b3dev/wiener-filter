bcb70860cf1396f806e2807d2e4e033e4a87679e
yurleb <ylebedev@borderstates.com>
2021-02-05 19:31:59 -0500

    implemented

--- main.py
+++ main.py
@@ -1,16 +1,169 @@
-# This is a sample Python script.
+import scipy.stats as stats
+import numpy as np
+from scipy.stats import levy_stable
+from scipy.signal import lfilter
+from numpy.fft import fft, fftshift, fftfreq
+import matplotlib.pylab as plt
+from scipy.linalg import toeplitz
+from numpy.linalg import inv
 
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import padasip as pa
+
+from correlation import xcorr
+
+plt.style.use('ggplot')  # nicer plots
+
+N0 = 1.5
+N = 10000
+t = np.arange(N)
+#alpha noise
+alpha = 1.8
+r = levy_stable.rvs(alpha, 1.0,0, size=N)
+#correlation
+Rbiased, lags = xcorr(r)
+#PSD
+Yf = fft(r)
+Py = 1 / N * abs(Yf)**2
+f = fftfreq(N)
+f = np.linspace(-0.5, 0.5, N)
+
+# given plant with H(z) = (1-z^-10)/(1-z^-1)
+yw = lfilter([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1], [1, -1], r)
+#white noise, gaussian with sigma^2 = N0/2
+XGauss = stats.norm(loc=0, scale=np.sqrt(N0/2))
+s = XGauss.rvs(size=N)
+#add white noise
+y = yw + s
+
+#correlation
+Rybiased, ylags = xcorr(y)
+#PSD
+Yyf = fft(y)
+Pyy = 1 / N * abs(Yyf)**2
+fy = np.linspace(-0.5, 0.5, N)
+
+# plot input/output
+fig, ax = plt.subplots(6, 1, figsize=(10, 15))
+ax[0].plot(t, r)
+ax[0].set_title("Alpha stable noise, alpha=1.6")
+ax[0].set_xlabel("Delay")
+ax[0].axis('tight')  #Tight layout of the axis
+
+# biased correlation
+ax[1].plot(lags, Rbiased)
+ax[1].set_title("Correlation function")
+ax[1].set_xlabel("Delay")
+ax[1].axis('tight')  #Tight layout of the axis
+
+# PSD
+ax[2].plot(f, fftshift(Py), alpha=0.65, label="Periodogram")
+ax[2].set_title("Input PSD")
+ax[2].axis('tight')  #Tight layout of the axis
+
+ax[3].plot(t, y)
+ax[3].set_title("Output with Gaussian White noise")
+ax[3].set_xlabel("Delay")
+ax[3].axis('tight')  #Tight layout of the axis
+
+# biased correlation
+ax[4].plot(ylags, Rybiased)
+ax[4].set_title("Correlation function")
+ax[4].set_xlabel("Delay")
+ax[4].axis('tight')  #Tight layout of the axis
+
+# PSD
+ax[5].plot(fy, fftshift(Pyy), alpha=0.65, label="Periodogram")
+ax[5].set_title("Output PSD")
+ax[5].axis('tight')  #Tight layout of the axis
+
+plt.legend()
+fig.tight_layout()
+plt.show()
+
+window=100
+u=r[0:window]
+d=y[0:window]
+qmax=15 # max value for q
+mmse=np.zeros(qmax) # initialize the vector of errors
+for q in range(0,qmax):
+    c=xcorr(u,u,maxlags=q)[0][q::] #correlation vector
+    Ruu=toeplitz(c)
+    Rdu=xcorr(d,u,maxlags=q)[0][q::]
+    w=inv(Ruu).dot(Rdu)
+    # Minimum error
+    sigma2d= np.mean(d ** 2)
+    mmse[q]=sigma2d-w.dot(Rdu)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+print("MMSE: ", mmse)
+
+plt.plot(range(0,qmax),mmse)
+plt.xlabel("Order of the filter")
+plt.ylabel("MMSE")
+plt.title("MMSE as a function of the length of the identification filter")
+plt.show()
+#figcaption("MMSE as a function of the length of the identification filter")
+
+zy=lfilter(w,[1],r)
+plt.plot(zy)
+plt.xlabel("Time")
+plt.title("Predicted")
+plt.show()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.plot(yw)
+plt.xlabel("Time")
+plt.title("Actual")
+plt.show()
+
+err = abs(pa.misc.logSE(yw,zy).min())
+print(err)
+plt.plot(pa.misc.logSE(yw,zy), "r", label="Squared error [dB]")
+plt.title("Filter error")
+# plt.xlabel("Number of iteration [-]")
+plt.show()
+
+#identification
+window = 30
+x = pa.input_from_history(r, n=window)
+f = pa.filters.FilterNLMS(mu=0.9, n=window)
+y = y[:-(window-1)]
+y1, e, w = f.run(y, x)
+
+# show results
+plt.figure(figsize=(12.5, 9))
+plt.subplot(221)
+plt.title("Target Signal")
+plt.xlabel("Number of iteration [-]")
+plt.plot(y, "b", label="d - target")
+plt.xlim(0, N)
+plt.legend()
+
+plt.subplot(222)
+plt.title("Adaptation")
+plt.xlabel("Number of iteration [-]")
+plt.plot(y, "b", label="d - target")
+plt.plot(y1, "g", label="y - output")
+plt.xlim(0, N)
+plt.legend()
+
+plt.subplot(223)
+plt.title("Predicted Signal")
+plt.xlabel("Number of iteration [-]")
+plt.plot(y1, "g", label="y - output")
+plt.xlim(0, N)
+plt.legend()
+
+plt.subplot(224)
+plt.title("Filter error")
+plt.xlabel("Number of iteration [-]")
+plt.plot(pa.misc.logSE(e), "r", label="Squared error [dB]")
+plt.legend()
+plt.xlim(0, N)
+plt.tight_layout()
+plt.show()
+print("And the resulting coefficients are: {}".format(w[-1]))
+
+err_lms = abs(pa.misc.logSE(e).min())
+print(err_lms)
