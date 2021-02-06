6cab2e68f5fcd0f43b2d93b307c17c769dbd2f38
yurleb <ylebedev@borderstates.com>
2021-02-05 21:44:17 -0500

    implemented

--- speach.py
+++ speach.py
@@ -4,6 +4,9 @@ from scipy.stats import levy_stable
 from scipy.signal import lfilter
 from numpy.fft import fft, fftshift, fftfreq
 import matplotlib.pylab as plt
+from scipy.io import wavfile
+import scipy
+import scipy.fftpack
 
 import padasip as pa
 
@@ -12,76 +15,68 @@ from correlation import xcorr
 
 plt.style.use('ggplot')  # nicer plots
 
-N0 = 0.1
-N = 10000
-t = np.arange(N)
-# alpha noise
-alpha = 1.8
-r = levy_stable.rvs(alpha, 1.0, 0, size=N)
-# correlation
-Rbiased, lags = xcorr(r)
-# PSD
-Yf = fft(r)
-Py = 1 / N * abs(Yf) ** 2
-f = fftfreq(N)
-f = np.linspace(-0.5, 0.5, N)
+fs_rate, signal = wavfile.read("sound.WAV")
+print ("Frequency sampling", fs_rate)
+l_audio = len(signal.shape)
+print ("Channels", l_audio)
+if l_audio == 2:
+    signal = signal.sum(axis=1) / 2
+N = signal.shape[0]
+print ("Complete Samplings N", N)
+secs = N / float(fs_rate)
+print ("secs", secs)
+Ts = 1.0/fs_rate # sampling interval in time
+print ("Timestep between samples Ts", Ts)
+t = scipy.arange(0, secs, Ts) # time vector as scipy arange field / numpy.ndarray
+FFT = abs(scipy.fft(signal))
 
-# given plant with H(z) = (1-z^-10)/(1-z^-1)
-yw = lfilter([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1], [1, -1], r)
-# white noise, gaussian with sigma^2 = N0/2
-XGauss = stats.norm(loc=0, scale=np.sqrt(N0 / 2))
-s = XGauss.rvs(size=N)
-# add white noise
-y = yw + s
+freqs = scipy.fftpack.fftfreq(signal.size, t[1]-t[0])
+fft_freqs = np.array(freqs)
+
+plt.subplot(211)
+p1 = plt.plot(t, signal, "g") # plotting the signal
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.subplot(312)
+p2 = plt.plot(freqs, FFT, "r") # plotting the complete fft spectrum
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Count dbl-sided')
+plt.show()
 
 # correlation
-Rybiased, ylags = xcorr(y)
+Rybiased, ylags = xcorr(signal)
 # PSD
-Yyf = fft(y)
+Yyf = fft(signal)
 Pyy = 1 / N * abs(Yyf) ** 2
 fy = np.linspace(-0.5, 0.5, N)
 
 # plot input/output
-fig, ax = plt.subplots(6, 1, figsize=(10, 15))
-ax[0].plot(t, r)
-ax[0].set_title("Alpha stable noise, alpha=1.6")
+fig, ax = plt.subplots(2, 1, figsize=(10, 15))
+# biased correlation
+ax[0].plot(ylags, Rybiased)
+ax[0].set_title("Correlation function")
 ax[0].set_xlabel("Delay")
 ax[0].axis('tight')  # Tight layout of the axis
 
-# biased correlation
-ax[1].plot(lags, Rbiased)
-ax[1].set_title("Correlation function")
-ax[1].set_xlabel("Delay")
+
+# PSD
+ax[1].plot(fy, fftshift(Pyy), alpha=0.65, label="Periodogram")
+ax[1].set_title("Input PSD")
 ax[1].axis('tight')  # Tight layout of the axis
 
-# PSD
-ax[2].plot(f, fftshift(Py), alpha=0.65, label="Periodogram")
-ax[2].set_title("Input PSD")
-ax[2].axis('tight')  # Tight layout of the axis
-
-ax[3].plot(t, y)
-ax[3].set_title("Output with Gaussian White noise")
-ax[3].set_xlabel("Delay")
-ax[3].axis('tight')  # Tight layout of the axis
-
-# biased correlation
-ax[4].plot(ylags, Rybiased)
-ax[4].set_title("Correlation function")
-ax[4].set_xlabel("Delay")
-ax[4].axis('tight')  # Tight layout of the axis
-
-# PSD
-ax[5].plot(fy, fftshift(Pyy), alpha=0.65, label="Periodogram")
-ax[5].set_title("Output PSD")
-ax[5].axis('tight')  # Tight layout of the axis
-
-plt.legend()
-fig.tight_layout()
 plt.show()
+
+#max power
+#pwr = Pyy.max()/fs_rate
+
+# Gaussian white with same power
+N0 = 0.1
+XGauss = stats.norm(loc=0, scale=np.sqrt(N0 / 2))
+x = XGauss.rvs(size=N)
 
 window = 100
 qmax = 15  # max value for q
-w, mmse = adaptive_wiener(15, window, r, y)
+w, mmse = adaptive_wiener(qmax, window, x, signal)
 
 print("MMSE: ", mmse)
 
@@ -91,21 +86,21 @@ plt.ylabel("MMSE")
 plt.title("MMSE as a function of the length of the identification filter")
 plt.show()
 
-zy = lfilter(w, [1], r)
+zy = window*lfilter(w, [1], x)
 
 # show results
 plt.figure(figsize=(12.5, 9))
 plt.subplot(221)
 plt.title("Target Signal")
 plt.xlabel("Number of iteration [-]")
-plt.plot(y, "b", label="d - target")
+plt.plot(signal, "b", label="d - target")
 plt.xlim(0, N)
 plt.legend()
 
 plt.subplot(222)
 plt.title("MMSE Adaptation")
 plt.xlabel("Number of iteration [-]")
-plt.plot(yw, "b", label="d - target")
+plt.plot(signal, "b", label="d - target")
 plt.plot(zy, "g", label="y - output")
 plt.xlim(0, N)
 plt.legend()
@@ -120,16 +115,19 @@ plt.legend()
 plt.subplot(224)
 plt.title("MMSE Filter error")
 plt.xlabel("Number of iteration [-]")
-plt.plot(pa.misc.logSE(yw, zy), "r", label="Squared error [dB]")
+plt.plot(pa.misc.logSE(signal, zy), "r", label="Squared error [dB]")
 plt.legend()
 plt.xlim(0, N)
 plt.tight_layout()
 
+plt.show()
+
+
 # identification
-window = 30
-x = pa.input_from_history(r, n=window)
+window = 500
+x = pa.input_from_history(x, n=window)
 f = pa.filters.FilterNLMS(mu=0.9, n=window)
-y = y[:-(window - 1)]
+y = signal[:-(window - 1)]
 y1, e, w = f.run(y, x)
 
 # show results
@@ -165,8 +163,3 @@ plt.xlim(0, N)
 plt.tight_layout()
 plt.show()
 print("And the resulting coefficients are: {}".format(w[-1]))
-
-err = abs(pa.misc.logSE(yw, zy).min())
-print("ERROR MSME: ", err)
-err_lms = abs(pa.misc.logSE(e).min())
-print("ERROR LMS: ", err_lms)
